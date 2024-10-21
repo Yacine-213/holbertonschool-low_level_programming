422bf4e11b9db8078c364128c6c1c5f74017e7ae
Yacine-213 <d.yacine2020@gmail.com>
2024-10-21 14:50:35 +0000

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints "Programming is like building a multilingual puzzle"
+ * using the puts function, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
     puts("\"Programming is like building a multilingual puzzle");
