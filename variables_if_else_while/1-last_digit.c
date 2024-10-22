b27754dc7594e48f0a4b1bd4dc8350e13db46798
Yacine-213 <d.yacine2020@gmail.com>
2024-10-22 21:22:04 +0000

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -5,30 +5,33 @@
 /**
  * main - Entry point of the program
  *
- * Description: This program assigns a random number to the variable n, 
- * extracts its last digit, and prints messages based on the value of the last digit.
+ * Description: This program generates a random number and
+ * prints the last digit with specific conditions.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
-        int last_digit;
+	int n;
 
-        srand(time(0));  /* Seed the random number generator */
-        n = rand() - RAND_MAX / 2;  /* Assign a random number to n */
+	srand(time(0)); /* Seed the random number generator */
+	n = rand() - RAND_MAX / 2; /* Generate a random number */
 
-        last_digit = n % 10;  /* Get the last digit */
+	int last_digit = n % 10; /* Get the last digit of n */
 
-        /* Print the last digit and the appropriate message */
-        printf("Last digit of %d is %d and ", n, last_digit);
-
-        if (last_digit > 5)
-                printf("is greater than 5\n");
-        else if (last_digit == 0)
-                printf("is 0\n");
-        else
-                printf("is less than 6 and not 0\n");
-
-        return (0);
-}	
+	/* Print the last digit with the specified conditions */
+	printf("Last digit of %d is %d", n, last_digit);
+	if (last_digit > 5)
+	{
+		printf(" and is greater than 5\n");
+	}
+	else if (last_digit == 0)
+	{
+		printf(" and is 0\n");
+	}
+	else
+	{
+		printf(" and is less than 6 and not 0\n");
+	}
+	return (0); /* Return success */
+}
