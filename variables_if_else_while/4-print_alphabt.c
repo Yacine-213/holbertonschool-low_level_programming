35696a0c2b4d024991bf505272304dddd52f85b3
Yacine-213 <d.yacine2020@gmail.com>
2024-10-23 13:42:35 +0000

    9-print_comb.c

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+
+{
+	char letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+		if (letter != 'q' && letter != 'e')
+	putchar('\n');
+	return (0);
+}
