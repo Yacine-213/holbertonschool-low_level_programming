774e43fb3901aa5c8510a9a9dc19200972d406bb
Yacine-213 <d.yacine2020@gmail.com>
2024-10-23 10:29:18 +0000

    2-print_alphabet.c

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -2,6 +2,14 @@
 #include <time.h>
 #include <stdio.h>
 
+/**
+ * main - Entry point of the program
+ *
+ * This function prints the lowercase alphabet followed by a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
 	char letter;
