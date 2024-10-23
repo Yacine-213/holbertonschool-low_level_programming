6d325b232d72b5bfd64376b876170cb4df685e65
Yacine-213 <d.yacine2020@gmail.com>
2024-10-23 13:51:59 +0000

    4-print_alphabt.c

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -5,14 +5,27 @@
 /**
  * main - Entry point of the program
  *
+ * This function prints the alphabet in lowercase,'q' and 'e',
+ * followed by a newline.
+ *
  * Return: Always 0 (Success)
  */
-int main(void)
 
+int main(void)
 {
-	char letter;
-	for (letter = 'a'; letter <= 'z'; letter++)
-		if (letter != 'q' && letter != 'e')
-	putchar('\n');
+	int i;
+
+	/* Loop through lowercase letters 'a' to 'z' */
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		/* Skip 'q' and 'e' */
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+
+	putchar('\n'); /* Print newline at the end */
+
 	return (0);
 }
