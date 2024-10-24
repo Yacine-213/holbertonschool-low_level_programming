c85cd8f7b3d65d82eb74a0ebde67e48c982d7ac9
Yacine-213 <d.yacine2020@gmail.com>
2024-10-24 12:30:48 +0000

    0-putchar.c

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints "_putchar" followed by a new line.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	/* Print each character of "_putchar" using putchar function */
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n'); /* Print a new line */
+
+	return (0); /* Return 0 to indicate successful execution */
+}
