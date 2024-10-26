9cabd75fb47aa1bdd7709258014306718a43e488
Yacine-213 <d.yacine2020@gmail.com>
2024-10-26 08:54:12 +0000

    3-islower.c

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -4,6 +4,7 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _putchar(char c);
-int _islower(int c);  /* Déclaration de la fonction _islower */
+int _islower(int c);
+int _isalpha(int c);  /* Déclaration de la fonction _isalpha */
 
 #endif /* MAIN_H */
