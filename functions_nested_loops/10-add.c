527e2a4391adb430d7d09795fcaa6e961be60100
Yacine-213 <d.yacine2020@gmail.com>
2024-10-26 12:15:02 +0000

    10-add.c

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - additionne deux entiers et retourne le résultat
+ * @a: premier entier
+ * @b: second entier
+ *
+ * Return: la somme de a et b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - vérifie la fonction add
+ *
+ * Return: Toujours 0
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
