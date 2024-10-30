fbc3b3c74ff19c17caa496cbfedb89be4dd5abd6
Yacine-213 <d.yacine2020@gmail.com>
2024-10-30 10:47:11 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,9 +8,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2) // On itère en ajoutant 2 à i
+	for (i = 0; str[i] != '\0'; i += 2) /* On itère en ajoutant 2 à i */
 	{
-		_putchar(str[i]); // Imprime le caractère courant
+		_putchar(str[i]); /* Imprime le caractère courant */
 	}
-	_putchar('\n'); // Imprime un saut de ligne à la fin
+	_putchar('\n'); /* Imprime un saut de ligne à la fin */
 }
