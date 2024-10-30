fd2dcf97e0fb1189262436e554b7cf441f842c06
Yacine-213 <d.yacine2020@gmail.com>
2024-10-30 10:52:33 +0000

    6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string
- * @str: the string to print
+ * puts2 - imprime chaque deuxième caractère d'une chaîne
+ * @str: la chaîne à imprimer
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2) /* On itère en ajoutant 2 à i */
+	/* Imprime chaque deuxième caractère */
+	for (i = 0; str[i] != '\0'; i += 2)
 	{
-		_putchar(str[i]); /* Imprime le caractère courant */
+		_putchar(str[i]);  /* Imprimer le caractère courant */
 	}
-	_putchar('\n'); /* Imprime un saut de ligne à la fin */
+	_putchar('\n');  /* Imprimer une nouvelle ligne à la fin */
 }
