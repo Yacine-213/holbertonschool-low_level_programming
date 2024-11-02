b9a1c0e5d75a877561444ac3a82d7e1ac6c8f89d
Yacine-213 <d.yacine2020@gmail.com>
2024-11-02 11:40:22 +0000

    4-rev_array.c

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -1,15 +1,39 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - teste la fonction print_rev
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to print
  *
- * Return: Toujours 0.
+ * Return: nothing.
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != 0)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+	}
+	printf("\n");
+}
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
  */
 int main(void)
 {
-	char *str;
+	int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-	print_rev(str);
+	print_array(a, sizeof(a) / sizeof(int));
+	reverse_array(a, sizeof(a) / sizeof(int));
+	print_array(a, sizeof(a) / sizeof(int));
 	return (0);
 }
