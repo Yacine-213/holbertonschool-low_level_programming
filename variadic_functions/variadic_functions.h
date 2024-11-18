740cd8f15ca196849237a3a7d8e03ba1199f7407
Yacine-213 <d.yacine2020@gmail.com>
2024-11-18 16:45:44 +0000

    1-print_numbers.c

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,9 +1,10 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 
-#include <stdarg.h>
+#include <stdarg.h>  /* For va_list, va_start, va_arg, va_end */
 
-int sum_them_all(const unsigned int n, ...);
+/* Function Prototypes */
+void print_numbers(const char *separator, const unsigned int n, ...);
 
-#endif
+#endif /* VARIADIC_FUNCTIONS_H */
 
