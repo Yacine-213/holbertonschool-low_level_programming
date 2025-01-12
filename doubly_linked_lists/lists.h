9e8f542438028d294b216f4f1a1143a8325d2502
Yacine-213 <d.yacine2020@gmail.com>
2025-01-12 22:07:00 +0000

    0

---
+++ doubly_linked_lists/lists.h
@@ -0,0 +1,25 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stddef.h>
+
+/**
+ * struct dlistint_s - Liste doublement chaînée
+ * @n: Entier stocké dans le nœud
+ * @prev: Pointeur vers le nœud précédent
+ * @next: Pointeur vers le nœud suivant
+ *
+ * Description: Structure pour une liste doublement chaînée
+ */
+typedef struct dlistint_s
+{
+    int n;
+    struct dlistint_s *prev;
+    struct dlistint_s *next;
+} dlistint_t;
+
+/* Prototypes des fonctions */
+size_t print_dlistint(const dlistint_t *h);
+
+#endif /* LISTS_H */
+
