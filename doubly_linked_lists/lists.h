ee38bb26d745a08480a89521fb021eff2c54d068
Yacine-213 <d.yacine2020@gmail.com>
2025-01-12 22:14:35 +0000

    3

--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -19,7 +19,7 @@ typedef struct dlistint_s
 } dlistint_t;
 
 /* Prototypes des fonctions */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 size_t print_dlistint(const dlistint_t *h);
 
 #endif /* LISTS_H */
